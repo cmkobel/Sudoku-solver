86704191ae9e25810bae3152cd4c39d61cb903c2
Carl Mathias Kobel <cmkobel@gmail.com>
2017-12-19 19:27:26 +0530

    comments translated to english

--- sudoku_grid.py
+++ sudoku_grid.py
@@ -2,10 +2,12 @@ import random
 import time
 random.seed("vivoksnekanogsåværebange")
 
+# Author: CMK
+
 '''
-??
-* undgå flere instantieringer! (tidbesparelse) ? -Det er vel ikke muligt hvis alle muligheder skal undersøges?
-* tupler eller lister?
+~ todo ~
+* tuples or lists?
+
 '''
 
 debug = False
@@ -25,74 +27,75 @@ class Sudoku_grid():
 	def __str__(self): 
 		result = "\nSolution: "
 		for row in range(9):
-			if row % 3 == 0: # separer kvadrater lodret
+			if row % 3 == 0: # seperate 3x3 squares horizontally
 				result += "\n"
 			for col in range(9):
 				if col % 3 == 0:
-					result += "  " # separer kvadrater vandret
-				result += str(self.problem[row][col]) + " " # indsæt tal
-			result += "\n" # skift linje efter hver række
+					result += "  " # seperate 3x3 squares vertically
+				result += str(self.problem[row][col]) + " " # write number
+			result += "\n" # return after each of the rows
 		return result
 
 	
-	# Finder koordinatet for det første nul.
+	# Finds the coordinate for the first zero (empty field)
 	def first_empty_pos(self):
 		for row in range(9):
 			for col in range(9):
 				if self.problem[row][col] == 0:
 					return (row, col)
-		return (9, 9)
+		return (9, 9) # (9, 9) doesn't exist as counting starts at 0. (9, 9) is simply a sign that there is no empty positions at all.
 
 
 	# These three related helper functions gather main structures for rule checking
-	# Return the row designated by the row_pointer
+	# Return the row designated by the row_pointer (1x9)
 	def row(self, row_in):
 		return (i for i in self.problem[row_in])
-	# Return the column
+	# Return the column (9x1)
 	def column(self, col_in):		
 		return (self.problem[i][col_in] for i in range(9))
-	# Return the 3x3 square
+	# Return the square (3x3)
 	def square(self, coord_in):
-		# build list:
+		# build two dimensional list:
 		list = ((self.problem[int(coord_in[0]/3)*3 + row][int(coord_in[1]/3)*3 + col] for col in range(3)) for row in range(3))
 		# flatten. (the 2d structure is not important)
 		return (item for sublist in list for item in sublist)
 
 
-	# Candidate gives the set of candidates for the pointed cell
+	# candidate() gives the set of possible candidates to fill in the pointed cell
 	def candidates(self):
 		# remove the numbers in the row, column and square. Left is the possible numbers for the spot
-		result = {i for i in range(1, 10)}
-		result -= set(tuple(self.row(self.row_pointer)))
-		result -= set(tuple(self.column(self.col_pointer))) 
-		result -= set(tuple(self.square([self.row_pointer, self.col_pointer])))
+		result = {i for i in range(1, 10)} # numbers 0 .. 9
+		result -= set(tuple(self.row(self.row_pointer))) # remove all numbers found in the associated row,
+		result -= set(tuple(self.column(self.col_pointer))) # column,
+		result -= set(tuple(self.square([self.row_pointer, self.col_pointer]))) # and square.
 		return result
 
 
 	# Returns true if all values are valid
+	# It is strictly only necessary to call this method in the beginning of the search, as the algorithm should not be able to insert in-valid entries.
 	def valid(self):
 		for row_i in range(9):
-			for col_i in range(9): # er det ikke nok at gå diagonalt ned igennem?
-				# fjern nuller fra listerne
+			for col_i in range(9):
+				# remove zeroes, as zero denotes a blank field.
 				row = (val for val in self.row(row_i) if val != 0)
 				col = (val for val in self.column(col_i) if val != 0)
 				square = (val for val in self.square([row_i, col_i]) if val != 0)
 				def has_duplicates(list):
-					# hvis længden af en liste er det samme som længden af settet af listen, kan der ikke være duplikater
+					# If the length of a list is the same as the length of the set of the list, no duplicates can exist
 					return len(list) != len(set(list))
 				if has_duplicates(row) or has_duplicates(col) or has_duplicates(square):
 					return False
-		# Hvis loopet har kørt igennem uden at finde nogle duplikater, må sudokuen være valid
+		# If the loop has run throughout without the finding of any duplicates, the sudoku must be valid 
 		return True
 
 
-	# depth first tree search
-	def search(self):
+	# depth first tree next
+	def next(self):
 		# When sudoku is created, first check if it is solved and valid (technically, invalid sudokus should be impossible at this stage)
 		if self.first_empty_pos() == (9, 9) and self.valid: # sudokuen er løst, base case
 			print(self)
 			if single_solution: quit()
-			return # og her vil jeg så gerne stoppe de andre
+			return # exit object in any case
 		# update pointer in the new sudoku_grid-object
 		zero_coord = self.first_empty_pos()
 		self.row_pointer, self.col_pointer = zero_coord[0], zero_coord[1]
@@ -103,14 +106,13 @@ class Sudoku_grid():
 		for candidate in random.sample(candidates_list, len(candidates_list)): # først herefter det skal være rekursivt.
 			#debug
 			if debug: print("\nCandidate", candidate, "of", self.candidates(), "in coordinate:", [self.row_pointer, self.col_pointer], end="")
-			# overfør grid og erstat candidate
+			# transfer grid and insert proposed candidate
 			child_problem = tuple(tuple(candidate if (i==self.row_pointer and j==self.col_pointer) else self.problem[i][j] for j in range(9)) for i in range(9))
-			#child_problem[self.row_pointer][self.col_pointer] = candidate # overfør kandidat til grid
-			Sudoku_grid(child_problem).search() # lav et nyt objekt for hver kandidat
+			Sudoku_grid(child_problem).next() # A new object is spawned for every candidate proposed
 
 
 def read_norvig2(input):
-	# brug evt map, så jeg ikke skal 
+	# use map()?
 	grid = tuple(tuple(int(input[row*9+col%9]) if input[row*9+col%9] != "." else 0 for col in range(9)) for row in range(9))
 	return grid
 
@@ -189,4 +191,4 @@ def read_norvig2(input):
 
 
 parent = Sudoku_grid(read_norvig2(".....6....59.....82....8....45........3........6..3.54...325..6.................."))
-parent.search()
+parent.next()
